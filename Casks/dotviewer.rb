b7dad892ae5cde4ce6d29c279f6ba231f4e4da42
Stian Larsen <stian.larsen@mac.com>
2026-04-22 14:20:19 +0200

    fix(cask): pass brew style + audit (uninstall order, alpha zap, drop redundant verified:)

--- Casks/dotviewer.rb
+++ Casks/dotviewer.rb
@@ -2,8 +2,7 @@ cask "dotviewer" do
   version "1.3.0"
   sha256 "89a2a6c0859b1bfd82dfc83e11d820b30e53416cb8d1e4db2883ee94e94946de"
 
-  url "https://github.com/stianlars1/dotViewer/releases/download/v#{version}/dotViewer-#{version}.dmg",
-      verified: "github.com/stianlars1/dotViewer/"
+  url "https://github.com/stianlars1/dotViewer/releases/download/v#{version}/dotViewer-#{version}.dmg"
   name "dotViewer"
   desc "Quick Look extension for syntax-highlighted source code previews"
   homepage "https://github.com/stianlars1/dotViewer"
@@ -31,14 +30,14 @@ cask "dotviewer" do
     system_command "/usr/bin/qlmanage", args: ["-r"], sudo: false
   end
 
-  uninstall quit:      "com.stianlars1.dotViewer",
-            launchctl: "com.stianlars1.dotViewer"
+  uninstall launchctl: "com.stianlars1.dotViewer",
+            quit:      "com.stianlars1.dotViewer"
 
   zap trash: [
+    "~/Library/Application Support/dotViewer",
+    "~/Library/Caches/com.stianlars1.dotViewer",
     "~/Library/Group Containers/group.stianlars1.dotViewer.shared",
     "~/Library/Preferences/com.stianlars1.dotViewer.plist",
-    "~/Library/Caches/com.stianlars1.dotViewer",
-    "~/Library/Application Support/dotViewer",
     "~/Library/Saved Application State/com.stianlars1.dotViewer.savedState",
   ]
 end
